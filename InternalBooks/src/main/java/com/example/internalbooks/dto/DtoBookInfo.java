ebc1405c48c1e3367eb8358ca025e21a4d2acf07
Grow.Kimata <momoseven-7@outlook.jp>
2025-11-07 15:21:47 +0900

    developの不必要なimport削除/書籍削除機能の実装完了

--- InternalBooks/src/main/java/com/example/internalbooks/dto/DtoBookInfo.java
+++ InternalBooks/src/main/java/com/example/internalbooks/dto/DtoBookInfo.java
@@ -2,8 +2,6 @@ package com.example.internalbooks.dto;
 
 import java.util.List;
 import lombok.Data;
-import java.util.Arrays;
-import java.util.ArrayList;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
